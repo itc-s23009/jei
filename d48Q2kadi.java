f9603a3fd59a7e98319a1b30b0c703b2497dd13f
itc-s23009 <s23009@std.it-college.ac.jp>
2024-05-24 12:31:31 +0900

    wei

--- d48Q2kadi.java
+++ d48Q2kadi.java
@@ -1,41 +1,51 @@
 // s23009
 // ポリモーフィズムの課題
 
-//スーパークラスの定義
+// スーパークラスの定義
+class Rion {
+	public void makeSound() {
+		System.out.println("pritl");
+	}
+}
+
+
 class Animal {
-    public void makeSound() {
-        System.out.println("");
-    }
+	public void makeSound() {
+		System.out.println("");
+	}
 
-    public void test() {
-        System.out.println("テスト出力");
-    }
+	public void test() {
+		System.out.println("テスト出力");
+	}
 }
 
-//継承したサブクラスDogの定義
+
+// 継承したサブクラスDogの定義
 class Dog extends Animal {
-    @Override
-    public void makeSound() {
-        System.out.println("ワンワン");
-    }
+	@Override
+	public void makeSound() {
+		System.out.println("ワンワン");
+	}
 }
 
-//継承したサブクラスCatの定義
+
+// 継承したサブクラスCatの定義
 class Cat extends Animal {
-				@Override
-				public void makeSound() {
-								System.out.println("ニャー");
-				}
+	@Override
+	public void makeSound() {
+		System.out.println("ニャー");
+	}
 }
+
 
 public class d48Q2kadi {
-    public static void main(String[] args) {
-        Animal a = new Dog(); // インスタンス化
-				Animal b = new Cat(); // インスタンス化
-        a.makeSound(); // どのような処理になるでしょう？
-        a.test(); // どのような処理になるでしょう
-				b.makeSound();
-				b.test();
-    }
+	public static void main(String[] args) {
+		Animal a = new Dog(); // インスタンス化
+		Animal b = new Cat(); // インスタンス化
+		a.makeSound(); // どのような処理になるでしょう？
+		a.test(); // どのような処理になるでしょう
+		b.makeSound();
+		b.test();
+	}
 }
 
